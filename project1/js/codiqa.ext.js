cc7f850b285fcdb173902bc4c152ba86dcd7d550
frankestein68k <nmackenz@Neils-MacBook-Pro.local>
2013-11-07 22:00:09 -0500

    Bringing the website closer to requirements
    IT'S THE FINAL COUNTDOWN!

---
+++ project1/js/codiqa.ext.js
@@ -0,0 +1,8 @@
+/*
+ * File name: codiq.ext.js
+ * Author: Neil MacKenzie
+ * Website name: Mobile Personal Portfolio site
+ * File description: Holds JavaScript code for site, empty at the moment
+ */
+
+// Put your custom code here
